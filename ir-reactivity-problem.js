a72f27f8bb31632cfba91478dd595194fbf00389
Tom Coleman <tom@thesnail.org>
2013-07-09 00:07:57 +1000

    More subtle problems

--- ir-reactivity-problem.js
+++ ir-reactivity-problem.js
@@ -1,10 +1,17 @@
 if (Meteor.isClient) {
   Router.map(function() {
-    this.route('hello', {path: '/'});
+    this.route('hello', {path: '/', layout: 'layout'});
   });
   
-  Template.hello.greeting = function() {
-    return Session.get('foo');
+  Template.layout.nav = function() {
+    return Session.get('nav');
+  }
+  
+  Template.hello.content = function() {
+    return Session.get('content');
+  }
+  Template.hello.aside = function() {
+    return Session.get('aside');
   }
 }
 
